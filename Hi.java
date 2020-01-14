da8e961eddee23372b1c4ac76f819444945e9505
Mila <morgenglow@gmail.com>
2020-01-14 22:33:06 +0300

    lesson 1

---
+++ Hi.java
@@ -0,0 +1,10 @@
+public class Hi {
+    //class MyNew
+    //var myVar
+    //method myMethod
+    //constants MAX_VALUE
+    //hot key psvm, sout, alt+insert, alt+enter
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
